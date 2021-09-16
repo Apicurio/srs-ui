102653b063a4ca39c381812aed8988bd6f08c4bf
ajaypratap003 <ajay.pratap233@gmail.com>
2021-09-16 16:56:44 +0530

    fix pagination reload page issue  and fix dev server issue

--- src/app/ServiceRegistry/ServiceRegistryFederated.tsx
+++ src/app/ServiceRegistry/ServiceRegistryFederated.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { ServiceRegistry } from './ServiceRegistry';
-import { RootModal } from '@app/components';
+import { RootModal, PaginationProvider } from '@app/components';
 import srsi18n from '@i18n/i18n';
 import { SharedContext, SharedContextrops } from '@app/context';
 
@@ -15,7 +15,9 @@ const ServiceRegistryFederated: React.FC<ServiceRegistryFederatedProps> = ({
     <I18nextProvider i18n={srsi18n}>
       <SharedContext.Provider value={{ preCreateInstance, shouldOpenCreateModal }}>
         <RootModal>
-          <ServiceRegistry />
+          <PaginationProvider>
+            <ServiceRegistry />
+          </PaginationProvider>
         </RootModal>
       </SharedContext.Provider>
     </I18nextProvider>
