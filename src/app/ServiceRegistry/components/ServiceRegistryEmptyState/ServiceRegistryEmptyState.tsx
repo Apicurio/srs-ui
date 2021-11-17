f84392ff2061af0cefc78c79eb38e58df64829e7
hemahg <hhg@redhat.com>
2021-11-17 17:25:12 +0530

    Adding Quickstart

--- src/app/ServiceRegistry/components/ServiceRegistryEmptyState/ServiceRegistryEmptyState.tsx
+++ src/app/ServiceRegistry/components/ServiceRegistryEmptyState/ServiceRegistryEmptyState.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { PageSection, ButtonVariant } from '@patternfly/react-core';
+import { Trans, useTranslation } from 'react-i18next';
+import { PageSection, ButtonVariant, Button } from '@patternfly/react-core';
 import { MASEmptyState, MASEmptyStateVariant } from '@app/components';
+import {
+  QuickStartContext,
+  QuickStartContextValues,
+} from '@patternfly/quickstarts';
 
 export type ServiceRegistryEmptyStateProps = {
   onCreateserviceRegistry: () => void;
@@ -10,13 +14,34 @@ export type ServiceRegistryEmptyStateProps = {
 const ServiceRegistryEmptyState: React.FC<ServiceRegistryEmptyStateProps> = ({ onCreateserviceRegistry }) => {
   const { t } = useTranslation();
 
+  const qsContext: QuickStartContextValues =
+    React.useContext(QuickStartContext);
+
   return (
     <PageSection padding={{ default: 'noPadding' }} isFilled>
       <MASEmptyState
         emptyStateProps={{ variant: MASEmptyStateVariant.NoItems }}
         titleProps={{ title: t('srs.empty_state_registry_title') }}
         emptyStateBodyProps={{
-          body: t('srs.empty_state_registry_description'),
+          body: (
+            <>
+              <Trans
+                i18nKey='srs.empty_state_registry_description'
+                components={[
+                  <Button
+                    variant={ButtonVariant.link}
+                    isSmall
+                    isInline
+                    key='btn-quick-start'
+                    onClick={() =>
+                      qsContext.setActiveQuickStart &&
+                      qsContext.setActiveQuickStart('getting-started-service-registry')
+                    }
+                  />,
+                ]}
+              />
+            </>
+          ),
         }}
         buttonProps={{
           title: t('srs.create_service_registry'),
